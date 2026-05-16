b2ab5c87d7682a7fb0c8d970da23588ec011426d
razhinav <razhina.vv@phystech.edu>
2026-05-16 12:48:35 +0300

    lil bug in ch fixed

--- ch.hpp
+++ ch.hpp
@@ -7,7 +7,7 @@ public:
     ~ChFunction() override;
     double exactValue(double x) const override;
     double firstTerm(double x) const override;
-    unsigned firstNonZeroDegree() const override;
+    unsigned firstDeg() const override;
     double nextTerm(double prevTerm, unsigned prevN, double x) const override;
     double maxDerivative(unsigned n, double x) const override;
 };
