728ba923a559343805f44455b6814c5ad37090b3
razhinav <razhina.vv@phystech.edu>
2026-05-16 12:11:19 +0300

    changing pow for reccurence

--- pow.hpp
+++ pow.hpp
@@ -3,18 +3,13 @@
 
 class PowerFunction : public TaylorFunction {
 private:
-    double alpha_; // Сохраняем показатель степени
-
-    // Вспомогательная функция для биномиального коэффициента C(alpha, n)
-    static double binomialCoeff(double alpha, unsigned n);
-
+    double alpha_; //показатель степени
 public:
     explicit PowerFunction(double alpha, double k);
-    ~PowerFunction() override = default; // Можно использовать default, так как нет сырых указателей
-
+    ~PowerFunction() override = default;
     double exactValue(double x) const override;
-    double maclaurinTerm(unsigned n, double x) const override;
-    
-    // Возвращает оценку максимума |f^(n)(xi)| на интервале [0, x] или [-|x|, |x|]
+    double firstTerm(double x) const override;
+    unsigned firstDeg() const override;
+    double nextTerm(double prevTerm, unsigned prevN, double x) const override;
     double maxDerivative(unsigned n, double x) const override;
 };
